22c52702b68ca5b0b9259cbfbd450d4d33584285
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-10 11:51:03 +0000

    Updated backend.tf

--- AzureInfra/backend.tf
+++ AzureInfra/backend.tf
@@ -10,7 +10,7 @@ backend "azurerm" {
     resource_group_name  = "tstate-rg"
     storage_account_name = "tfstr8811"
     container_name       = "tfstate"
-    key                 = "mediawiki.tfstate"
+    key                 = "mediawiki_b.tfstate"
     access_key           = "W0q2+9GdV0Dh9Hjy3uGIK49RWnhYy9D3/kpvEtPD+ENt9ghTQqWz07zwDyXKy4HGkc+D1EYH3iAk+AStfV6zGw=="
 
   }
