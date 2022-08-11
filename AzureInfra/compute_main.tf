865e4fe650795cf714d9fb53cf91f676bbbe58a1
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-11 03:32:38 +0000

    Updated compute_main.tf

--- AzureInfra/compute_main.tf
+++ AzureInfra/compute_main.tf
@@ -105,8 +105,8 @@ network_profile {
 resource "azurerm_virtual_machine_scale_set_extension" "apphealthext" {
 name                         = "example"
 virtual_machine_scale_set_id = azurerm_linux_virtual_machine_scale_set.appvmss.id
-publisher = “Microsoft.ManagedServices”
-type = “ApplicationHealthLinux”
+publisher ="Microsoft.ManagedServices"
+type = "ApplicationHealthLinux"
 auto_upgrade_minor_version = true
 }
 
