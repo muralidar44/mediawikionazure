794d3752b5e7a31b2a752cf6b112b0f8d4bd3ff0
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-11 02:52:49 +0000

    Updated compute_main.tf

--- AzureInfra/compute_main.tf
+++ AzureInfra/compute_main.tf
@@ -60,7 +60,7 @@ resource "azurerm_virtual_machine" "dbvm" {
   }
 
   # required when using rolling upgrade policy
-  health_probe_id = azurerm_lb_probe.lbprobe.id
+  //health_probe_id = azurerm_lb_probe.lbprobe.id
     sku {
     name     = "Standard_D4s_v3"
     tier     = "Standard"
