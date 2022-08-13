1192c320f68bccc9c92304ec4795707c73eb9294
Muralidar Sripadha <muralidar.sripadha@outlook.com>
2022-08-13 08:16:25 +0000

    Added backend.tf

---
+++ AzureInfra/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.17.0"
+    }
+  }
+
+backend "azurerm" {
+    resource_group_name  = "tstate-rg"
+    storage_account_name = "tfstr8811"
+    container_name       = "tfstate"
+    key                 = "mediawiki_b.tfstate"    
+
+  }
+}
